20661764b5c31eeace83ec216366275894142ec0
Moni <monika.berke@gmail.com>
2022-10-24 06:18:46 +0200

    1 hiba van benne

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import {HumanHero} from "./hero";
+import {TransformerHero} from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {id: 1, name: "A", sex: "male", age: 30, health: 15, },
+    {id: 2, name: "B", sex: "female", age: 30, health: 25, },
+    {id: 3, name: "C", sex: "male", age: 40, health: 35, },
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {id: 1, name: "Mittomen", wings: 0, wheels: 4, clan: "Transformers" },
+    {id: 2, name: "Izeke", wings: 2, wheels: 4, clan: "Transformers" },
+    {id: 3, name: "Hogyishivjak", wings: 2, wheels: 0, clan: "Transformers" },
+];
